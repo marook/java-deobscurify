64ce2ceccd97b75e785c395dbed462ac9aadd462
Markus Pielmeier <markus.pielmeier@gmx.de>
2011-04-20 14:16:18 +0200

    added some GPL headers

--- src/test/java/com/github/marook/java_deobscurify/compare/imports/ImportsArtifactComparatorTest.java
+++ src/test/java/com/github/marook/java_deobscurify/compare/imports/ImportsArtifactComparatorTest.java
@@ -1,3 +1,23 @@
+/*
+ *
+ * Copyright 2011 Markus Pielmeier
+ *
+ * This file is part of java-deobscurify.
+ *
+ * java-deobscurify is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * java-deobscurify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with java-deobscurify.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package com.github.marook.java_deobscurify.compare.imports;
 
 import java.io.IOException;
